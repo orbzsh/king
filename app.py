e87c717e6618915a95519c9f11ddc9e5b013af3a
root <root@localhost.localdomain>
2015-05-05 23:31:32 +0800

    change index

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Index page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
